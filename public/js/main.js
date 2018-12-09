74f6c4724c2d8e65807a4a8777ec50f480b68774
Shayant Sital <shayant@gmail.com>
2018-12-09 17:55:20 -0300

    added search url

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ fetchUsers().then(data => {
     document.createTextNode(`Total SU Github Users: ${data.total_count} 🇸🇷`)
   );
   users = data.items;
-  console.log(users);
+  console.log(data);
 
   users.forEach(user => {
     const li = document.createElement("li");
